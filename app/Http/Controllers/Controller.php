87f1557a5d91b87dc6b1108fb377508f9d45cba0
pyridz <49193952+pyridz@users.noreply.github.com>
2025-02-25 19:16:23 +0100

    Set up Vue.js and create Home.vue

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,19 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Inertia\Inertia;   
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function render($component, $props = [])
+    {
+        return Inertia::render($component, $props);
+    }
 }
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gradient-to-r from-blue-500 to-purple-500 min-h-screen flex items-center justify-center">
+      <h1 class="text-white text-4xl font-bold">Welcome to My Portfolio</h1>
+    </div>
+  </template>
